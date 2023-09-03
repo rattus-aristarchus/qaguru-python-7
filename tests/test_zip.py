c369b87a44431f7da00e74a0d044a2301a5febf5
rattus-aristarchus <lankin.m.a@gmail.com>
2023-09-03 23:22:01 +0300

    archive fix

--- tests/test_zip.py
+++ tests/test_zip.py
@@ -11,19 +11,16 @@ def test_zip():
     file_1 = os.path.join(RESOURCES_DIR, XLS_NAME)
     file_2 = os.path.join(RESOURCES_DIR, XLSX_NAME)
     zip_path = os.path.join(RESOURCES_DIR, ZIP_NAME)
-    filenames = [file_0, file_1, file_2]
+    paths_to_files = [file_0, file_1, file_2]
 
     with zipfile.ZipFile(zip_path, mode="w") as archive:
-        for filename in filenames:
+        for filename in paths_to_files:
             archive.write(filename)
 
         names_in_zip = archive.namelist()
+        filenames = [PDF_NAME, XLS_NAME, XLSX_NAME]
 
         for name in names_in_zip:
-            assert get_proper_name(name) in filenames
+            assert os.path.basename(name) in filenames
 
         os.remove(zip_path)
-
-
-def get_proper_name(name):
-    return os.path.join(os.path.abspath(os.sep), name)
